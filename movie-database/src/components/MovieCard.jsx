cdc226731e637c15b73f82e292a97f490aba86f5
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-01 02:48:13 +0300

    feat(MovieCard): component created and coded to accept a props parameter which is the fetched movies state passed from the parent component.

--- movie-database/src/components/MovieCard.jsx
+++ movie-database/src/components/MovieCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const MovieCard = () => {
-  return (
-    <div>MovieCard</div>
-  )
-}
+
+const MovieCard = (props) => {
+      return (
+          <div className='grid grid-cols-2  sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 '>
+              {props.movies.map((movie, index) => (
+                  <div key={index} className = 'flex flex-col items-center justify-center gap-4   transition  ease-in-out  hover:-translate-y-1 hover:scale-110  hover:opacity-90  duration-300 	'>
+                      <img src={movie.Poster} alt='movie poster not found' className='rounded-2xl hover:opacity-90'></img>
+                      <h2>{movie.Title}</h2>
+                      <p>{movie.Year}</p>
+                  </div>
+              ))}
+          </div>
+      );
+  };
+
 
 export default MovieCard
