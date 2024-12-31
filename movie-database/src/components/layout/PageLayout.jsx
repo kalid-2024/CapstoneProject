c0a6f8cbcded7ce0b442fe5299998df6af4390b0
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-01 02:50:25 +0300

    feat(PageLayout):created PageLayout , imported Outlet from react-router-dom for the layout

--- movie-database/src/components/layout/PageLayout.jsx
+++ movie-database/src/components/layout/PageLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../common/Navbar'
 import Footer from '../common/Footer'
-import { Outlet } from 'react-router-dom' 
+import { Outlet } from 'react-router-dom'
+import  '../../index.css'
 
 
 
 const PageLayout = () => {
   return (
-    <div>
+    <div className=' h-lvh'>
         <Navbar />
         <Outlet />
         <Footer />
