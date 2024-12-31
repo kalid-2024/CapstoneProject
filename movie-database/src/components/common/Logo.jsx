76ecb38404b11a2e104f18edc6b2d70df5d10dfd
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-01 02:59:53 +0300

    feat(Logo): a Logo component created and styled with tailwind

---
+++ movie-database/src/components/common/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const Logo = () => {
+  return (
+     <div  className='px-4 text-3xl  px-4 py-4  rounded-full hover:bg-slate-300' >
+          <a href="/">
+          <h1 className="text-green-600  sm:text-3xl  px-2 font-black  ">&#127909;    MovieLovers</h1>
+          </a>
+      </div> 
+  );
+}
+
+export default Logo
