fa5e7a533b301c9bddef96e76cd4ad3058a39ef3
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-01 02:38:31 +0300

    feat(router):created a router managing component

---
+++ movie-database/src/router/router.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom';
+import PageLayout from '../components/layout/PageLayout';
+import LandingPage from '../components/Pages/LandingPage';
+import About from '../components/Pages/About';
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <PageLayout/> ,
+      children: [
+        {
+          path: '/',
+          index: true,
+          element: <LandingPage/> ,
+        },
+        {
+          path: "/About",
+          element: <About/>,
+        },
+        // {
+        //   path:"/catagory/action",
+        //   element: action ,
+        // },
+        // {
+        //   path:"/catagory/comedy",
+        //   element: comedy 
+        // },
+        // {
+        //   path:"/catagory/drama",
+        //   element: drama 
+        // },
+        // {
+        //   path:"/catagory/sci-fi",
+        //   element: sci-fi 
+        // },
+        // {
+        //   path:"/favorites",
+        //   element: favorites 
+        // },
+      ],
+    },
+
+  ])
+
+
+
+
+
+
+export default router
