b310984935bade3aa0091fc0acafab9ddf0382c9
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-01 02:59:21 +0300

    feat(SearchBar):created and syled a search bar and also added a logic for state handling and form submision handling

--- movie-database/src/components/form/SearchBar.jsx
+++ movie-database/src/components/form/SearchBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
 
-export default SearchBar
+  const SearchBar = ({ onSearch }) => {
+
+    const [query, setQuery] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setQuery(e.target.value)
+      if (query.trim()) {
+        onSearch(query);
+      }
+    };
+  
+    return (
+      <form onSubmit={handleSubmit} className="flex mx-auto items-center justify-center gap-4 mb-6">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          value={query}
+          onChange={handleSubmit}
+          className="input input-bordered w-100 md:w-1/3 lg:w-1/3 sm:text-base text-sm blur-xs font-bold py-3 px-4 pl-12 pr-10 rounded-full bg-gray-100 bg-opacity-20  placeholder-theme-adaptive focus:outline-none focus:ring-2 focus:ring-primary/30"
+        />
+        <button type="submit" onClick={handleSubmit} className="bg-green-600 text-white px-4 py-2 rounded-full hover:opacity-90">
+          Search
+        </button>
+      </form>
+    );
+  };
+  
+  export default SearchBar;
+  
