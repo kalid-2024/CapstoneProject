676deb6190e7e00eb5946eec8a83ee77e4387eaa
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-01 02:46:21 +0300

    feat(update): added state managment for the movies,searchValue,isLoading,errror states.  implemented useEffect and useState hooks for state and effects managment. SearchBar component imported in the herocontent and passed onSearch props,conditonal rendering applie for the states. the MovieCard component was called and passed the  movies state to it. Styles are also added

--- movie-database/src/components/Pages/LandingPage.jsx
+++ movie-database/src/components/Pages/LandingPage.jsx
@@ -1,24 +1,99 @@
-import React from 'react'
-import Hero from '../common/Hero'
+import {React, useState, useEffect} from 'react'
 import Layout from '../layout/Layout'
-
+import empty from '../../assets/images/empty.png'
+import SearchBar from '../form/SearchBar'
+import {fetchMovies , fetchMovieDetail} from '../Services/Api'
+import MovieCard from '../MovieCard'
+import errorimage from '../../assets/images/error.png'
 
 
 const LandingPage = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState(" ");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+
+  useEffect(()=>{
+
+    const fetchData = async (searchValue)=> {
+      setIsLoading(true);
+      try{
+      const data = await fetchMovies(searchValue);
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError('');
+      }else {
+        setError(data.Error);
+        setMovies([]);
+        }
+      }catch(error){
+        setError("We couldn't find any movies or TV shows matching your search.");
+      }finally{
+        setIsLoading(false);
+      }
+    }
+
+    if(searchValue){
+        fetchData(searchValue);
+      }
+    },[searchValue]);
+
+    // Handle user search input
+  const handleSearch = (query) => {
+    setSearchValue(query);
+      // setSelectedMovie(null); // Reset selected movie when a new search is performed
+    };
+
+
   return (
   <Layout 
     heroContent={
-      <div className="text-center"> 
-        <h1>What would you like to watch now?</h1>
-        <p>Your ultimate movie and Tv show search engine.</p>
-        <p>Search Bar</p>
+      <div className="text-center mb-48 "> 
+        <h1 className='text-green-600  text-6xl mt-16 font-bold'>What would you like to watch now?</h1>
+        <p className='text-green-600 text-2xl mt-8 mb-8 font-bold'>Your ultimate movie and Tv show search engine.</p>
+        
+       <SearchBar  onSearch={handleSearch} />
+
+
+        <div className='flex flex-wrap justify-center gap-2 mt-4 max-w-[600px] mx-auto'>
+          <select className=" text-black  border rounded-xs w-auto min-w-[120px]  font-semibold">
+            <option value>Media Type</option>
+            <option value="movie">Movie</option>
+            <option value="tv">Tv Show</option>
+          </select>
+          <select className=" text-black  border rounded-xs w-auto min-w-[120px]  font-semibold">
+            <option value>Genre</option>
+            <option value="28">Action</option>
+            <option value="35">Comedy</option>
+            <option value="18">Drama</option>
+          </select>
+          <select className=" text-black  border rounded-xs w-auto min-w-[120px]  font-semibold">
+            <option value="vote_average.desc">Highest Rating</option>
+            <option value="vote_average.asc">Lowest Rating</option>
+          </select>
+        </div>
+
       </div>
     }
     pageContent={
-      <p>Coming Soon!</p>
+      <div className='mb-60 text-center text-green-600 font-bold'>
+              { error ? (<div> 
+                <img src={errorimage} alt="search result not found!" className='w-24 h-24 sm:w-32 sm:h-32  mx-auto mb-4'/>
+              <p>{error}</p>
+              </div>)  : isLoading ? (<p>Loading...</p>) : movies.length >0? (
+              <MovieCard movies={movies}/>
+            ):(
+            <div>
+                  <img src={empty} alt="No Search Results" className='w-24 h-24 sm:w-32 sm:h-32  mx-auto mb-4'/>
+                  <p>Start searching for movies or TV shows to see results here.</p>
+            </div>
+            )}
+    </div>
     }
-  />
-  )
+    />
+  );
 }
 
 export default LandingPage
