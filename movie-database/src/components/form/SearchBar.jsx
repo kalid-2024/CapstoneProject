fdb660955f78985e33c987429ba78ec570249988
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-02 00:54:16 +0300

    feat(modified): the searchbar width at certain screen sizes

--- movie-database/src/components/form/SearchBar.jsx
+++ movie-database/src/components/form/SearchBar.jsx
@@ -20,7 +20,7 @@ import React, { useState } from 'react';
           placeholder="Search movies..."
           value={query}
           onChange={handleSubmit}
-          className="input input-bordered w-100 md:w-1/3 lg:w-1/3 sm:text-base text-sm blur-xs font-bold py-3 px-4 pl-12 pr-10 rounded-full bg-gray-100 bg-opacity-20  placeholder-theme-adaptive focus:outline-none focus:ring-2 focus:ring-primary/30"
+          className="input input-bordered  md:w-1/3  lg:w-1/3 sm:text-base text-sm blur-xs font-bold py-3 px-4 pl-12 pr-10 rounded-full bg-gray-100 bg-opacity-20  placeholder-theme-adaptive focus:outline-none focus:ring-2 focus:ring-primary/30"
         />
         <button type="submit" onClick={handleSubmit} className="bg-green-600 text-white px-4 py-2 rounded-full hover:opacity-90">
           Search
