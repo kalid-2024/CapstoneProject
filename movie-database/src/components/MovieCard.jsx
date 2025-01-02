b0a7c01f78a67e2dc4915efab3f6ba7d05d676c3
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-03 00:35:14 +0300

    feat(update): removed props from the parameters and added movies props

--- movie-database/src/components/MovieCard.jsx
+++ movie-database/src/components/MovieCard.jsx
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom'
 
 
 
-const MovieCard = (props) => {
+const MovieCard = ({ movies }) => {  
       return (
           <div className='grid grid-cols-2  sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 '>
-              {props.movies.map((movie) => (
-                  <Link  to={`/MovieDetails/${movie.imdbID}`}  key={movie.imdbID} className = 'flex flex-col items-center justify-center gap-4   transition  ease-in-out  hover:-translate-y-1 hover:scale-110  hover:opacity-90  duration-300 	'>
+            
+              {movies.map((movie,index) => (
+                  <Link  to={`/MovieDetails/${movie.imdbID}`}  key={index} className = 'flex flex-col items-center justify-center gap-4   transition  ease-in-out  hover:-translate-y-1 hover:scale-110  hover:opacity-90  duration-300 '>
                       <img src={movie.Poster} alt='movie poster not found' className='rounded-3xl hover:opacity-90'></img>
                       <h2>{movie.Title}</h2>
                       <p>{movie.Year}</p>
