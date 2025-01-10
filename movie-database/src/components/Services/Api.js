c3a9be9e255e079f05fa806f103f1e7ccef8fbf7
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-10 18:19:18 +0300

    feat(modified): page to currentpage

--- movie-database/src/components/Services/Api.js
+++ movie-database/src/components/Services/Api.js
@@ -5,9 +5,9 @@ const API_KEY = 'c5cd2a06';
 const BASE_URL = 'http://www.omdbapi.com/';
 
 
-export const fetchMovies = async (query, page=1)=> {
+export const fetchMovies = async (query, currentPage=1)=> {
     try{
-        const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&s=${query}&page=${page}`);
+        const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&s=${query}&currentPage=${currentPage}`);
         return response.data ;
      }catch(error){
         console.error("We couldn't find any movies or TV shows matching your search.", error);
