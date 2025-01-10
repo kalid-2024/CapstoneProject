3ecfc860b8f1b972afa99e55d72812f2e1e007fe
kalid-2024 <kalidmohammed236@gmail.com>
2025-01-11 01:52:36 +0300

    feat(modified):Api.js

--- movie-database/src/components/Services/Api.js
+++ movie-database/src/components/Services/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL= process.env.REACT_APP_BASE_URL;
+
+const API_KEY = import.meta.env.VITE_API_KEY;;
+const BASE_URL= 'http://www.omdbapi.com/'
 
 
 
